a3b78da4088fcf2af00479ee1f51cf88de14fb2c
andrewtchan <andrew2020chan@gmail.com>
2023-10-25 20:51:02 -0700

    feat: add connection to Atlas
    may need to move the .connect call to a different file later on

--- packages/express-backend/src/backend.js
+++ packages/express-backend/src/backend.js
@@ -1,11 +1,22 @@
 import express from "express";
 import cors from "cors";
+import mongoose from "mongoose";
 
 const app = express();
 const port = 8000;
 
 app.use(cors());
 app.use(express.json());
+
+const connection_url =
+  "mongodb+srv://newUser:newPassword@cluster0.g8i3gad.mongodb.net/FlixDB?retryWrites=true&w=majority";
+
+mongoose
+  .connect(connection_url, {
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to FlixDB"))
+  .catch((error) => console.log(error));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
