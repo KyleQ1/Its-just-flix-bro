a3d8fdb62a87302323c1e2ac472a876ff65de70b
JefferNg <jefferng11@gmail.com>
2023-10-29 22:37:45 -0700

    added netflix, account, search icon

--- packages/react-frontend/src/App.js
+++ packages/react-frontend/src/App.js
@@ -1,12 +1,20 @@
 import logo from "./assets/netflix-logo.png";
+import accountIcon from "./assets/account-icon.png"
+import search from "./assets/search-icon.png"
 import "./index.css";
 
 function App() {
   return (
     <div>
-      <img src={logo} />
-      <h1>"Hello World!"</h1>
-      <button>My Button</button>
+      <header>
+        <p>
+          <img src={logo} align="left"/>
+          <img src={accountIcon} align="right"/>
+          <img src={search} align="right"/>
+          <center><b><h2>This is the header</h2></b></center>
+        </p>
+        <hr></hr>
+      </header>
     </div>
   );
 }
