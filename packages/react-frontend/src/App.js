dbf777747bdc5e9aec48436fdd1e70e65c1275ae
JefferNg <jefferng11@gmail.com>
2023-10-30 16:22:03 -0700

    placed code into component

--- packages/react-frontend/src/App.js
+++ packages/react-frontend/src/App.js
@@ -1,24 +1,10 @@
-import logo from "./assets/netflix-logo.png";
-import accountIcon from "./assets/account-icon.png"
-import search from "./assets/search-icon.png"
 import "./index.css";
+import NavBar from "./NavBar";
 
 function App() {
   return (
     <div>
-      <header>
-          <img src={logo} align="left"/>
-          <img src={accountIcon} align="right"/>
-          <img src={search} align="right"/>
-      </header>
-      <table width="30%">
-            <tr>
-              <td width="20%">Home</td>
-              <td>TV Shows</td>
-              <td width="20%">Movies</td>
-              <td>My List</td>
-            </tr>
-          </table>
+      <NavBar />
     </div>
   );
 }
---
+++ packages/react-frontend/src/NavBar.js
@@ -0,0 +1,27 @@
+import React from "react";
+import logo from "./assets/netflix-logo.png";
+import accountIcon from "./assets/account-icon.png"
+import search from "./assets/search-icon.png"
+
+function NavBar() {
+    return(
+        <div>
+            <header>
+            <img src={logo} align="left"/>
+            <img src={accountIcon} align="right"/>
+            <img src={search} align="right"/>
+        </header>
+        <table width="30%">
+                <tr>
+                <td width="20%">Home</td>
+                <td>TV Shows</td>
+                <td width="20%">Movies</td>
+                <td>My List</td>
+                </tr>
+            </table>
+        </div>
+    )
+    
+}
+
+export default NavBar
