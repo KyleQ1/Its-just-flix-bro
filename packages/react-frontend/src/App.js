0ec7fc87761ecaa63acef2604484e4f6bc3c3aa5
JefferNg <jefferng11@gmail.com>
2023-11-03 16:58:43 -0700

    implemented list of temp movies

--- packages/react-frontend/src/App.js
+++ packages/react-frontend/src/App.js
@@ -1,10 +1,14 @@
 import "./index.css";
 import NavBar from "./NavBar";
+import Movie from "./Movie"
 
 function App() {
   return (
     <div id="landing">
       <NavBar />
+      <Movie genre="Action"/>
+      <Movie genre="Comedy"/>
+      <Movie genre="Horror"/>
     </div>
   );
 }
