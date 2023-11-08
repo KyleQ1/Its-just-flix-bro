b3f6a39a52d2f292a30f24773a37b04c0fddcd05
KyleQ1 <kylequinlan09@gmail.com>
2023-11-08 15:54:45 -0800

    Cleanup chatgpt code closes #3

--- packages/express-backend/src/chatgptgetter.js
+++ packages/express-backend/src/chatgptgetter.js
@@ -10,27 +10,22 @@ if (result.error) {
 }
 const openai = new OpenAI();
 
-class openAI {
-  static defaultSystem = `You are a movie reviewer 
+defaultSystem = `You are a movie reviewer 
     and you are going to give a review for the movie given. If you 
     don’t know the movie just give a review to the best of your knowledge.
     If it's not out then continue `;
-  async sendPrompt(string, system = openAI.defaultSystem) {
-    const completion = await openai.chat.completions.create({
-      messages: [
-        { role: "system", content: system },
-        { role: "user", content: string },
-      ],
-      model: "gpt-3.5-turbo",
-    });
-    return completion.choices[0];
-  }
+// Takes in movietitle and takes a few minutes to get a response
+// Add response to database
+export default async function sendPrompt(
+  string,
+  system = openAI.defaultSystem,
+) {
+  const completion = await openai.chat.completions.create({
+    messages: [
+      { role: "system", content: system },
+      { role: "user", content: string },
+    ],
+    model: "gpt-3.5-turbo",
+  });
+  return completion.choices[0].message.content;
 }
-
-function main() {
-  const ai = new openAI();
-  ai.sendPrompt("What do you think of the five nights at freddys movie?")
-    .then((res) => console.log(res))
-    .catch((error) => console.error(error));
-}
-main();
