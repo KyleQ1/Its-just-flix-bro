53d23659f676c499fc4068759729535e51f922ef
andrewtchan <andrew2020chan@gmail.com>
2023-11-15 13:59:53 -0800

    feat: CSS styling for MovieCard

--- packages/react-frontend/src/MovieCard.js
+++ packages/react-frontend/src/MovieCard.js
@@ -5,9 +5,11 @@ import "./MovieCard.css";
 export default function MovieCard({ title, image, id }) {
   return (
     <div className="movie-card">
-      <Link to={`/movies/${id}`}>
-        <img src={image} alt={title} className="movie-image" />
-      </Link>
+      <div className="movie-image">
+        <Link to={`/movies/${id}`}>
+          <img src={image} alt={title} />
+        </Link>
+      </div>
     </div>
   );
 }
