5eb50b9c151cb1ee13cb7fcb3bdaa5af34fc81de
JefferNg <JefferNg@users.noreply.github.com>
2023-11-18 00:40:46 +0000

    style: format files

--- packages/react-frontend/src/MovieReviewPage.js
+++ packages/react-frontend/src/MovieReviewPage.js
@@ -3,31 +3,31 @@ import Header from "./Header";
 import "./MovieReviewPage.css";
 
 function MovieReviewPage(props) {
-    return (
-        <div id="landing">
-            <div id="movie-background">
-                <Header />
-                <h2>{/*props.title*/}title</h2>
-            </div>
-            <div id="review-body">
-                <div id="movie-info">
-                    <div id="popularity">
-                        <h4>1</h4>
-                        <h4>2</h4>
-                        <h4>3</h4>
-                        <h4>4</h4>
-                        <h4>5</h4>
-                    </div>
-                    <div id="genre">genre</div>
-                    <div id="description">description</div>
-                    <div id="release">release date</div>
-                </div>
-                <div id="movie-reviews">
-                    <h1>Review</h1>
-                    <button>Submit Review</button>
-                </div>
-            </div>
+  return (
+    <div id="landing">
+      <div id="movie-background">
+        <Header />
+        <h2>{/*props.title*/}title</h2>
+      </div>
+      <div id="review-body">
+        <div id="movie-info">
+          <div id="popularity">
+            <h4>1</h4>
+            <h4>2</h4>
+            <h4>3</h4>
+            <h4>4</h4>
+            <h4>5</h4>
+          </div>
+          <div id="genre">genre</div>
+          <div id="description">description</div>
+          <div id="release">release date</div>
         </div>
+        <div id="movie-reviews">
+          <h1>Review</h1>
+          <button>Submit Review</button>
+        </div>
+      </div>
+    </div>
   );
 }
 
