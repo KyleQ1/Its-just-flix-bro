f93b71ab585d76770d859065d769bcbf8ae093a1
KyleQ1 <KyleQ1@users.noreply.github.com>
2023-11-21 07:55:15 +0000

    style: format files

--- packages/react-frontend/src/pages/LoginPage.js
+++ packages/react-frontend/src/pages/LoginPage.js
@@ -3,7 +3,7 @@ import logo from "../assets/netflix.png";
 import "./LoginPage.css";
 
 const Login = () => {
-	const [signIn, setSignIn] = useState(false);
+  const [signIn, setSignIn] = useState(false);
   const [username, setUsername] = useState("");
 
   const handleUsernameChange = (e) => {
@@ -18,36 +18,40 @@ const Login = () => {
     <div className="login">
       <div className="login_background">
         <img className="login_logo" src={logo} alt="Netflix logo" />
-				<button className="login_button">Sign In</button>
-				<div className="login_gradient" />
+        <button className="login_button">Sign In</button>
+        <div className="login_gradient" />
       </div>
-			{signIn ? (
-					<h1>HI</h1>
-			) : (
-				<div className="login_body">
-					<h1>Unlimited movies, TV shows, and more.</h1>
-					<h2>Watch anywhere. Cancel anytime.</h2>
-					<h3>
-						Ready to watch? Enter your email to create or restart your membership.
-					</h3>
-					<div className="login_input">
-						<form onSubmit={handleSubmit}>
-							<input
-								type="email"
-								placeholder="Email address"
-								value={username}
-								onChange={handleUsernameChange}
-							/>
-							<button
-							onClick={() => setSignIn(true)}
-							className="login_getStarted" type="submit">
-								GET STARTED
-							</button>
-						</form>
-					</div>
-				</div>
-			)};
-		</div>
+      {signIn ? (
+        <h1>HI</h1>
+      ) : (
+        <div className="login_body">
+          <h1>Unlimited movies, TV shows, and more.</h1>
+          <h2>Watch anywhere. Cancel anytime.</h2>
+          <h3>
+            Ready to watch? Enter your email to create or restart your
+            membership.
+          </h3>
+          <div className="login_input">
+            <form onSubmit={handleSubmit}>
+              <input
+                type="email"
+                placeholder="Email address"
+                value={username}
+                onChange={handleUsernameChange}
+              />
+              <button
+                onClick={() => setSignIn(true)}
+                className="login_getStarted"
+                type="submit"
+              >
+                GET STARTED
+              </button>
+            </form>
+          </div>
+        </div>
+      )}
+      ;
+    </div>
   );
 };
 
