1b365f325ff0ad1ce7730dcc71d6e313bb9c451e
Kyle Quinlan <35123887+KyleQ1@users.noreply.github.com>
2023-11-21 03:44:20 -0800

    Fix dumb mistake

---
+++ packages/react-frontend/src/HomePage.js
@@ -0,0 +1,17 @@
+import React from "react";
+import NavBar from "./NavBar";
+import MovieList from "./MovieList";
+import "./index.css";
+
+function HomePage() {
+  return (
+    <div id="landing">
+      <NavBar />
+      <MovieList genre="Action" />
+      <MovieList genre="Comedy" />
+      <MovieList genre="Horror" />
+    </div>
+  );
+};
+
+export default HomePage;
