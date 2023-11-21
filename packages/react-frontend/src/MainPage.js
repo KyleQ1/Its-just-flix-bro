f5ce7ad4ef1af6bad9245ddb5f6cb60330bc449a
Kyle Quinlan <35123887+KyleQ1@users.noreply.github.com>
2023-11-21 03:11:00 -0800

    Run formatter

--- packages/react-frontend/src/MainPage.js
+++ packages/react-frontend/src/MainPage.js
@@ -3,12 +3,12 @@ import NavBar from "./NavBar";
 import MovieList from "./MovieList";
 
 const MainPage = () => {
-	<div id="landing">
-		<NavBar />
-		<MovieList genre="Action" />
-		<MovieList genre="Comedy" />
-		<MovieList genre="Horror" />
-	</div>
+  <div id="landing">
+    <NavBar />
+    <MovieList genre="Action" />
+    <MovieList genre="Comedy" />
+    <MovieList genre="Horror" />
+  </div>;
 };
 
 export default MainPage;
