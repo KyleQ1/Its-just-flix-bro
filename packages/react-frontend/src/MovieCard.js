19dae6b39fe694fd6333537345a9d4d7787ed457
Kyle Quinlan <35123887+KyleQ1@users.noreply.github.com>
2023-11-20 20:16:03 -0800

    Movie popout (#48)
    Add movie popout closes #13, 38

--- packages/react-frontend/src/MovieCard.js
+++ packages/react-frontend/src/MovieCard.js
@@ -1,15 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./MovieCard.css";
 
-export default function MovieCard({ title, image, id }) {
+export default function MovieCard({ title, image, genres, id }) {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
+
   return (
-    <div className="movie-card">
+    <div
+      className="movie-card"
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
       <div className="movie-image">
         <Link to={`/movies/${id}`}>
           <img src={image} alt={title} />
         </Link>
       </div>
+      {isHovered && (
+        <div className="movie-title">
+          <Link to={`/movies/${id}`} style={{ color: `white` }}>
+            {title}
+          </Link>
+          <p>{genres.join(", ")}</p>
+        </div>
+      )}
     </div>
   );
 }
