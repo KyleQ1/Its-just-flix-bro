9a6059070f72d8b936cf75f27578727c33b52df9
Kyle Quinlan <35123887+KyleQ1@users.noreply.github.com>
2023-11-22 04:49:11 -0800

    Create authentication to backend

--- packages/react-frontend/src/App.js
+++ packages/react-frontend/src/App.js
@@ -1,26 +1,35 @@
 import "./index.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AuthProvider, RequireAuth } from "react-auth-kit";
 import HomePage from "./pages/HomePage";
 import ProfilePage from "./pages/ProfilePage";
 import MovieReviewPage from "./pages/MovieReviewPage";
 import Login from "./pages/LoginPage";
 
 function App() {
-  const user = null;
-
   return (
-    <BrowserRouter>
-      <Routes>
-        {!user ? (
-          // redirect to login
-          <Route path="" element={<Login />} />
-        ) : (
-          <Route path="" element={<HomePage />} />
-        )}
-        <Route path="profile" element={<ProfilePage />} />
-        <Route path="movie" element={<MovieReviewPage />} />
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider
+      authType={"cookie"}
+      authName={"_auth"}
+      cookieDomain={window.location.hostname}
+      cookieSecure={window.location.protocol === "https:"}
+    >
+      <BrowserRouter>
+        <Routes>
+          <Route path="login" element={<Login />} />
+          <Route
+            path="/"
+            element={
+              <RequireAuth loginPath={"login"}>
+                <HomePage />
+              </RequireAuth>
+            }
+          />
+          <Route path="profile" element={<ProfilePage />} />
+          <Route path="movie" element={<MovieReviewPage />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
