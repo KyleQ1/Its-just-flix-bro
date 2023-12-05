18c4520c3f9e0a6b708e1d1dad8e0d6480d29ee0
hjohns50 <henryjohnson@comcast.net>
2023-12-04 19:31:01 -0800

    push to remote

---
+++ packages/express-backend/src/test/movie.test.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+import MovieModel from "../models/movie.js";
+
+// Mock the mongoose save method
+jest.mock('mongoose', () => ({
+  Schema: jest.fn(),
+  model: jest.fn(),
+}));
+
+describe('Movie Model Tests', () => {
+  test('Testing CREATE MOVIE -- SUCCESS', async () => {
+    // Mock the movie data
+    const movieData = {
+      title: 'Test Movie',
+      description: 'A test movie',
+      image: 'test-image.jpg',
+      genres: ['Action', 'Adventure'],
+      popularity: 8.5,
+      releaseDate: '2023-01-01',
+      reviews: [],
+    };
+    // Mock the movie instance
+    const movieInstance = new mongoose.Schema(movieData);
+    // Perform the test
+    const result = await MovieModel.createMovie(movieData);
+    // Assertions
+    expect(result).toEqual(movieInstance);;
+  });
+
+  // Add more tests for other scenarios (e.g., error cases)
+});
