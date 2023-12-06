26b129ebcc4d71334c4637ef9919d0b83b789839
andrewtchan <andrew2020chan@gmail.com>
2023-12-06 15:10:42 -0800

    login page text

--- packages/react-frontend/src/pages/LoginPage.js
+++ packages/react-frontend/src/pages/LoginPage.js
@@ -31,9 +31,9 @@ const Login = () => {
         ) : (
           <>
             <h1>Unlimited movies, TV shows, and more.</h1>
-            <h2>Watch anywhere. Cancel anytime.</h2>
+            <h2>Review anywhere. Cancel anytime.</h2>
             <h3>
-              Ready to watch? Enter your email to create or restart your
+              Ready to review? Enter your email to create or restart your
               membership.
             </h3>
             <div className="login_input">
