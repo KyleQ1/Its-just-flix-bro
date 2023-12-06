72784c78653f24bda0dc827421b1d13da066da1c
JefferNg <JefferNg@users.noreply.github.com>
2023-12-06 01:34:12 +0000

    style: format files

--- packages/react-frontend/src/App.js
+++ packages/react-frontend/src/App.js
@@ -24,7 +24,7 @@ function App() {
             path="/"
             element={
               //<RequireAuth loginPath={"login"}>
-                <HomePage />
+              <HomePage />
               //</RequireAuth>
             }
           />
